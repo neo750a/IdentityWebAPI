427adc94d69863d7c2b0e42c516a4c0a062cc0c6
Leonardo Mastri <neo750a@yahoo.com>
2020-02-12 16:56:57 -0700

    Testing out Totp from otp.net

---
+++ TOTPProject/Controllers/TotpController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OtpNet;
+using TOTPProject.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TotpController : ControllerBase
+    {
+        private Totp _totpManager;
+        private DafaqModel dafaq;
+
+        public TotpController()
+        {
+            _totpManager = new Totp(KeyGeneration.GenerateRandomKey());
+
+            dafaq = new DafaqModel();
+        }
+
+        [HttpGet]
+        public IActionResult OnGet()
+        {
+            //first test to verify right away
+            dafaq.codeOne = _totpManager.ComputeTotp(DateTime.UtcNow);
+
+            long hasBeenVerifiedBefore = 0;
+
+            dafaq.totpTestOne = _totpManager.VerifyTotp(dafaq.codeOne, out hasBeenVerifiedBefore);
+
+            dafaq.timesCodeOneBeenVerified = hasBeenVerifiedBefore;
+
+            //create new code to be past into Post Method
+
+            dafaq.codeTwo = _totpManager.ComputeTotp(DateTime.UtcNow);
+
+            return Ok(dafaq);
+        }
+
+        [HttpPost]
+        public IActionResult OnPost([FromBody]TotpModel input)
+        {
+            dafaq.codeTwo = input.code;
+
+            long hasBeenVerifiedBefore = 0;
+
+            dafaq.totpTestTwo = _totpManager.VerifyTotp(dafaq.codeTwo, out hasBeenVerifiedBefore);
+
+            dafaq.timesCodeTwoBeenVerified = hasBeenVerifiedBefore;
+
+            return Ok(dafaq);
+        }
+    }
+}
---
+++ TOTPProject/Models/DafaqModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TOTPProject.Models
+{
+    public class DafaqModel
+    {
+        public bool totpTestOne { get; set; }
+        public bool totpTestTwo { get; set; }
+        public string codeOne { get; set; }
+        public string codeTwo { get; set; }
+        public long timesCodeOneBeenVerified { get; set; }
+        public long timesCodeTwoBeenVerified { get; set; }
+    }
+}
